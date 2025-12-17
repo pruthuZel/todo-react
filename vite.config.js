faae3ec62900462059e91ff68b3952ee26549931
pruthuZel <pruthuZel@example.com>
2025-12-17 08:00:12 +0000

    DEV: Added fixes to allow host

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-
-  // Base path should be root for custom domain deployments
   base: '/',
-
   preview: {
-    host: true,            // bind to 0.0.0.0
-    port: 8080,            // server port
+    host: true,
+    port: 8080,
     allowedHosts: [
       'demo.condense.zeliot.in'
     ]
