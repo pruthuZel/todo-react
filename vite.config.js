eb7bec1d5f509030d3cc5167f68d3842cda629fa
pruthuZel <pruthuZel@example.com>
2025-12-17 07:49:21 +0000

    DEV: added vite build fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mdn.github.io/todo-react/"
+
+  // Base path should be root for custom domain deployments
+  base: '/',
+
+  preview: {
+    host: true,            // bind to 0.0.0.0
+    port: 8080,            // server port
+    allowedHosts: [
+      'demo.condense.zeliot.in'
+    ]
+  }
 })
